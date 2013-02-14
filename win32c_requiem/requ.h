82090cbf947b95317fab676ec75c0a3e45e30e6a
horong54 <k502000@naver.com>
2013-02-15 04:09:14 +0900

    vc project init

---
+++ win32c_requiem/requ.h
@@ -0,0 +1,21 @@
+#ifndef __REQU_H__
+#define __REQU_H__
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/*******************************************************************************************
+	definitions
+********************************************************************************************/
+
+#define BOOL		int
+#define DQUOTE		'"'
+#define	EOL			'\n'	/*define end of line */
+#define EQUAL		0
+#define	FALSE		0
+#define FAIL		-1
+
+
+
+#endif
